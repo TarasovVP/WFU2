2912a1a0bbcf00c48abb1b1c242bfb5bc5c954d7
TarasovVP <vptarasov1998@gmail.com>
2018-12-14 22:41:41 +0200

    some refactoring

--- app/src/main/java/com/gmail/tarasov1998/wfu2/GetWeathear.java
+++ app/src/main/java/com/gmail/tarasov1998/wfu2/GetWeathear.java
@@ -13,43 +13,16 @@ public class GetWeathear {
 
         Location loc = new Location();
 
-        JSONObject coordObj = getObject("coord", jObj);
-        loc.setLatitude(getFloat("lat", coordObj));
-        loc.setLongitude(getFloat("lon", coordObj));
 
-        JSONObject sysObj = getObject("sys", jObj);
-        loc.setCountry(getString("country", sysObj));
-        loc.setSunrise(getInt("sunrise", sysObj));
-        loc.setSunset(getInt("sunset", sysObj));
         loc.setCity(getString("name", jObj));
         weather.location = loc;
 
         JSONArray jArr = jObj.getJSONArray("weather");
 
-
         JSONObject JSONWeather = jArr.getJSONObject(0);
-        weather.currentCondition.setWeatherId(getInt("id", JSONWeather));
-        weather.currentCondition.setDescr(getString("description", JSONWeather));
-        weather.currentCondition.setCondition(getString("main", JSONWeather));
-        weather.currentCondition.setIcon(getString("icon", JSONWeather));
 
         JSONObject mainObj = getObject("main", jObj);
-        weather.currentCondition.setHumidity(getInt("humidity", mainObj));
-        weather.currentCondition.setPressure(getInt("pressure", mainObj));
-        weather.temperature.setMaxTemp(getFloat("temp_max", mainObj));
-        weather.temperature.setMinTemp(getFloat("temp_min", mainObj));
         weather.temperature.setTemp(getFloat("temp", mainObj));
-
-
-        JSONObject wObj = getObject("wind", jObj);
-        weather.wind.setSpeed(getFloat("speed", wObj));
-        weather.wind.setDeg(getFloat("deg", wObj));
-
-
-        JSONObject cObj = getObject("clouds", jObj);
-        weather.clouds.setPerc(getInt("all", cObj));
-
-
 
         return weather;
     }
@@ -66,9 +39,4 @@ public class GetWeathear {
     private static float  getFloat(String tagName, JSONObject jObj) throws JSONException {
         return (float) jObj.getDouble(tagName);
     }
-
-    private static int  getInt(String tagName, JSONObject jObj) throws JSONException {
-        return jObj.getInt(tagName);
-    }
-
 }
